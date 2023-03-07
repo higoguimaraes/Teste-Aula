e73d42e589c6164a79250dcd36694adeba39fb0f
Higo Guimaraes <higosguimaraes@gmail.com>
2023-03-06 21:05:47 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function modificaClasse() {
+    document.getElementById('background').className = 'estilo1'
+}
+function modificaClasse2() {
+    document.getElementById('background').className = 'estilo2'
+}
+function modificaClasse3() {
+    document.getElementById('background').className = 'estilo3'
+}
+function modificaClasse4() {
+    document.getElementById('background').className = 'estilo4'
+}
